1741a245239f23793ea152b99fb720520c200df7
kirsalfetnik <oriole001@gmail.com>
2022-04-20 20:26:22 +0200

    Create a mongoose schema for posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 
 const App = () => {
     return (
-        <div>
-            <h1>Application</h1>
-        </div>
+        <Container maxwidth="lg ">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">
+                    Opportunities
+                </Typography>
+                <img src={opportunities} alt="opportunities" height="60" />
+            </AppBar>
+        </Container>
     );
 }
 
