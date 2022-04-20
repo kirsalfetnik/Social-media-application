07afb277aacc751352b011e66e6fd34189d75a09
kirsalfetnik <oriole001@gmail.com>
2022-04-20 19:44:55 +0200

    Create main files and establish a server

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://kirillsalfetnik:8Y8gfyE4v5H16ZwA@cluster0.ojqes.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.port || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+// mongoose.set("useFindAndModify", false);
